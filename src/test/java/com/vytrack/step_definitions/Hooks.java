c5d1e34a42f28a1632f8fa3bd77559b9f606d1c0
MehmetAkifTUNC <kilyneu@gmail.com>
2020-11-01 20:32:04 +0100

    remote_chrome for hub with ip

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,6 +1,6 @@
 package com.vytrack.step_definitions;
 
-import com.vytrack.utilities.Driver;
+import  com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
